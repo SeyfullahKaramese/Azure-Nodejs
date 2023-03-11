8c29c533242ea276cb913d5627c0465c1f87ec2c
Seyfullah KARAMEŞE <seyfullahkaramese@gmail.com>
2023-03-11 14:28:47 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+
+
+
+app.use(express.json());
+
+app.get("/api/test", (req, res) => {
+    res.json({ message: "API request successful..." });
+
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log("Server Running.. "))
